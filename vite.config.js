8eced237748f8094127c31bf4e2b9d1506023f31
ml-michael <michael.zm@medialand.tw>
2022-03-23 15:29:28 +0800

    update vite-config to reslove path

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@
 
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import path, { resolve } from "path";
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -38,8 +38,11 @@ export default ({ mode }) => {
     resolve: {
       alias: [
         {
-          find: "@",
-          replacement: path.resolve(__dirname, "src"),
+          // find: "@",
+          // replacement: path.resolve(__dirname, "src"),
+          "@": resolve(__dirname, "src"),
+          views: resolve(__dirname, "src/views"),
+          utils: resolve(__dirname, "src/utils"),
         },
       ],
     },

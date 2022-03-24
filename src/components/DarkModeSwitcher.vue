bfbd9653526e341c9eaa94edbe3e783b6d34b1c1
ml-michael <michael.zm@medialand.tw>
2022-03-24 18:41:03 +0800

    create darkModeSwitcher

--- src/components/DarkModeSwitcher.vue
+++ src/components/DarkModeSwitcher.vue
@@ -1,12 +1,67 @@
 <template>
-  <div>DarkModeSwitcher</div>
+  <label class="block">
+    <input type="checkbox" class="sr-only flag-1" v-model="checked" />
+    <div
+      :class="[
+        'box-content h-10 w-20 rounded-full border-4',
+        'bg-yellow-400 flag-1-checked:bg-blue-900',
+        'flag-1-checked:border-gray-600',
+        'transition-all duration-300',
+      ]"
+    >
+      <div
+        :class="[
+          'h-full w-1/2 scale-75 rounded-full',
+          'translate-x-0 flag-1-checked:translate-x-full',
+          'bg-white/75 flag-1-checked:bg-white/50',
+          'transition-all duration-300',
+          'flex items-center justify-center',
+        ]"
+      >
+        <!-- 兩組 svg 設定數值是一樣的，所以可以合併 svg -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          :class="['h-7 w-7', 'text-yellow-500 flag-1-checked:text-white']"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <!-- Sun -->
+          <path
+            class="transition-all duration-300 flag-1-checked:opacity-0"
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          />
+          <!-- Moon -->
+          <path
+            class="opacity-0 transition-all duration-300 flag-1-checked:opacity-100"
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+          />
+        </svg>
+      </div>
+    </div>
+  </label>
 </template>
 
 <script>
 export default {
   name: "DarkModeSwitcher",
   data() {
-    return {};
+    return {
+      checked: false,
+    };
+  },
+  watch: {
+    checked() {
+      document.body.classList.toggle("dark");
+    },
+  },
+  mounted() {
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (matchMedia.matches) {
+      this.checked = true;
+    }
   },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const flagger = require("tailwind-flagger");
+
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html,pug}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flagger],
 };

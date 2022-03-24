1bdb0759e9c367df5683c95ac78f53fac083f3b3
ml-michael <michael.zm@medialand.tw>
2022-03-24 16:07:51 +0800

    create nextMonth

--- src/components/SimpleCalendar.vue
+++ src/components/SimpleCalendar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col overflow-hidden rounded-md border">
     <div
-      class="flex items-center justify-center border-b bg-blue-600 p-3 font-bold text-white"
+      class="flex items-center justify-center border-b bg-pink-600 p-3 font-bold text-white"
     >
-      2021 9 月
+      2022 3 月
     </div>
     <div class="grid grid-cols-7 place-items-center">
       <div
@@ -18,11 +18,35 @@
       <div
         :class="[
           'h-10 w-10',
+          'text-sm text-gray-400',
+          'flex items-center justify-center',
+          'rounded-full transition-all',
+        ]"
+        v-for="(day, index) in lastMonth"
+        :key="day"
+      >
+        {{ day }}
+      </div>
+      <div
+        :class="[
+          'h-10 w-10',
           'text-sm',
           'flex items-center justify-center',
           'rounded-full transition-all',
         ]"
-        v-for="(day, index) in 30"
+        v-for="(day, index) in thisMonth"
+        :key="day"
+      >
+        {{ day }}
+      </div>
+      <div
+        :class="[
+          'h-10 w-10',
+          'text-sm text-gray-400',
+          'flex items-center justify-center',
+          'rounded-full transition-all',
+        ]"
+        v-for="(day, index) in nextMonth"
         :key="day"
       >
         {{ day }}
@@ -37,8 +61,47 @@ export default {
   data() {
     return {
       weekDays: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+      year: 2022,
+      month: 3,
     };
   },
+
+  computed: {
+    lastMonth() {
+      const days = [];
+
+      const current = new Date(this.year, this.month - 1, 1);
+      const wd = current.getDay();
+
+      for (let i = wd; i > 0; i--) {
+        const temp = new Date(current);
+        temp.setDate(current.getDate() - i);
+        days.push(temp.getDate());
+      }
+
+      return days;
+    },
+    thisMonth() {
+      const days = [];
+      //   const count = new Date(2022, 3, 0).getDate();
+      const current = new Date(this.year, this.month, 0);
+      for (let index = 1; index <= current.getDate(); index++) {
+        days.push(index);
+      }
+      return days;
+    },
+    nextMonth() {
+      const days = [];
+      //   const count = new Date(2022, 3, 0).getDate();
+      const current = new Date(this.year, this.month + 1, 0);
+      for (let index = 1; index <= current.getDate(); index++) {
+        days.push(index);
+      }
+      return days;
+    },
+  },
+
+  mounted() {},
 };
 </script>
 

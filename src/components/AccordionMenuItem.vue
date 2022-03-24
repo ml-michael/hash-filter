ac605d758dbaf142a606ec8921f21aad7711a0cc
ml-michael <michael.zm@medialand.tw>
2022-03-24 14:44:31 +0800

    add according styles

--- src/components/AccordionMenuItem.vue
+++ src/components/AccordionMenuItem.vue
@@ -1,5 +1,50 @@
 <template>
-  <div></div>
+  <div class="w-80">
+    <label
+      :class="[
+        'px-4 py-2',
+        'border border-gray-300',
+        'hover:bg-pink-300',
+        'flex items-center justify-between',
+        'cursor-pointer',
+        'transition-all',
+      ]"
+    >
+      <div>選項</div>
+      <div
+        :class="[
+          'h-9 w-9',
+          'hover:bg-gray-200',
+          'rounded-full',
+          'flex items-center justify-center',
+          'transition-all',
+        ]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          :class="['h-6 w-6', 'transition-all']"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </div>
+    </label>
+    <div
+      :class="[
+        'border border-t-0 border-gray-300',
+        'overflow-hidden transition-all',
+      ]"
+    >
+      <div class="p-4 text-gray-600">內容</div>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/views/component-demo.vue
+++ src/views/component-demo.vue
@@ -4,6 +4,8 @@
     <h1 :class="['text-center text-5xl', 'font-bold text-pink-500']">
       component-demo
     </h1>
+
+    <AccordionMenuItem :class="['mx-auto', 'my-5', 'text-white']" />
   </div>
   <SectionPaddingBottom />
 </template>
@@ -11,10 +13,11 @@
 <script>
 import SectionPaddingTop from "../components/SectionPaddingTop.vue";
 import SectionPaddingBottom from "../components/SectionPaddingBottom.vue";
+import AccordionMenuItem from "../components/AccordionMenuItem.vue";
 export default {
   name: "component-demo",
   data() {},
-  components: { SectionPaddingTop, SectionPaddingBottom },
+  components: { SectionPaddingTop, SectionPaddingBottom, AccordionMenuItem },
 };
 </script>
 

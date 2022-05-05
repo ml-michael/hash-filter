f030ffe2104b5f5776fff21a1b0b90b5d6176d08
ml-michael <michael.zm@medialand.tw>
2022-05-06 04:56:14 +0800

    新增 vue-select 範例

---
+++ src/views/selectMap.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div class="container">
+
+        <div>
+            <v-select label="name" :reduce="
+                (option) => {
+                    citySelect = option.districts;
+                    selected = option.districts[0] ? option.districts[0].name : '';
+                }
+            " placeholder="縣市" :options="cities"></v-select>
+        </div>
+        <div>
+            <v-select label="name" placeholder="鄉鎮區域" v-model="selected" :options="citySelect"></v-select>
+        </div>
+    </div>
+</template>
+<script>
+import { ref } from "vue";
+import cities from "@/json/TwCities.json";
+import 'vue-select/dist/vue-select.css';
+
+
+export default {
+    setup() {
+        const citySelect = ref([]);
+        const selected = ref("");
+        return { citySelect, cities, selected };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+div {
+    // color: #;
+    border: #fff;
+    background-color: #fff;
+}
+</style>

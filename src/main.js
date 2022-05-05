d4376dce9b63918487164743a2b15d308bd21122
ml-michael <michael.zm@medialand.tw>
2022-05-06 04:54:23 +0800

    import instagramEmbed vSelect

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import Router from "./router";
+import InstagramEmbed from "vue-instagram-embed";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
-createApp(App).use(Router).mount("#app");
+createApp(App)
+  .use(Router)
+  .use(InstagramEmbed)
+  .component("v-select", vSelect)
+  .mount("#app");
